e9b69a955b0e21af838d8688e1aacdaad3044a1b
uladzislau <1vladislad1@gmail.com>
2022-09-07 22:05:09 +0200

    broken products

--- assets/src/script.js
+++ assets/src/script.js
@@ -49,5 +49,124 @@ if (iconMenu) {
 // Data
 
 
-fetch
+fetch("../assets/src/products.json")
+.then(response => response.json())
+.then(products => {
+    localStorage.setItem("products", JSON.stringify(products))
+});
+
+let container = document.querySelector(".produkty__content");
+let loadMoreButton = document.querySelector(".produkty__content button");
+
+let initialItems = 4;
+let loadItems = 4;
+
+
+function loadInitialItems() {
+    let products = JSON.parse(localStorage.getItem("products"));
+    let out = "";
+    let counter = 0;
+    for(let product of Object.keys(products)){
+        if(counter < initialItems){
+            out += `
+                <div class="produkty__item">
+                    <div class="produkty__flags">
+                        <p class="produkty__flag produkty__flag_1">${product.flags}</p>
+                        <p class="produkty__flag produkty__flag_2">${product.flags}</p>
+                    </div>
+                    <img src="${product.imgSrc}" alt="2" class="produkty__img produkty__img_1">
+                    <a href="#" class="produkty__text">
+                        ${product.title}
+                    </a>
+                    <div class="produkty__down-card">
+                        <div class="produkty__the-down-card">
+                            <p class="produkty__dispozice">
+                                ${product.availability}
+                            </p>
+                            <p class="produkty__cena">
+                                ${product.price} CZK
+                            </p>
+                        </div>
+                        <button class="produkty__add-to">
+                            <a href="#">
+                                <img src="/assets/img/shoppingCart.svg" alt="shoppingCart" class="produkty__add-to_icon">
+                            </a>
+                        </button>
+                    </div>
+                </div>
+            `;
+        }
+        counter++;
+    }
+
+    let div = document.createElement("div");
+    container.insertBefore(div, loadMoreButton);
+    div.innerHTML = out;
+}
+
+
+function loadData(){
+    let products = JSON.parse(localStorage.getItem("products"));
+    let currentDisplayedItems = document.querySelectorAll(".product").length;
+
+    let out = "";
+    let counter = 0;
+    for(let product of Object.keys(products)){
+        if(counter >= currentDisplayedItems && counter < loadItems + currentDisplayedItems){
+            out += `
+                <div class="produkty__item">
+                <div class="produkty__flags">
+                    <p class="produkty__flag produkty__flag_1">${product.flags}</p>
+                    <p class="produkty__flag produkty__flag_2">${product.flags}</p>
+                </div>
+                <img src="${product.imgSrc}" alt="2" class="produkty__img produkty__img_1">
+                <a href="#" class="produkty__text">
+                    ${product.title}
+                </a>
+                <div class="produkty__down-card">
+                    <div class="produkty__the-down-card">
+                        <p class="produkty__dispozice">
+                            ${product.availability}
+                        </p>
+                        <p class="produkty__cena">
+                            ${product.price} CZK
+                        </p>
+                    </div>
+                    <button class="produkty__add-to">
+                        <a href="#">
+                            <img src="/assets/img/shoppingCart.svg" alt="shoppingCart" class="produkty__add-to_icon">
+                        </a>
+                    </button>
+                </div>
+            </div>
+            `;
+        }
+        counter++;
+    }
+
+    let div = document.createElement("div");
+    container.insertBefore(div, loadMoreButton);
+    div.innerHTML = out;
+    div.style.opacity = 0;
+
+
+    if(document.querySelectorAll(".product").length == products.length){
+        loadMoreButton.style.display = "none"
+    }
+
+    fadeIn(div);
+}
+
+
+function fadeIn(div){
+    let opacity = 0;
+    let interval = setInterval(function(){
+        if (opacity <= 1) {
+            opacity = opacity + 0.1;
+            div.style.opacity = opacity;
+        }else{
+            clearInterval(interval);
+        }
+    }, 30);
+}
 
